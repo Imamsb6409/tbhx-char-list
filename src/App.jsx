99811b00b19396ada12c688956a65e608d2f4bf5
IMAMSB6409 <imam0406@student.abudzar.sch.id>
2026-01-22 10:50:04 +0700

    nih

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/container/Header";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Haii
-    </h1>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
---
+++ src/components/container/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <>
+      <div className="w-[42vw] bg-black h-10 mt-2 left-0 absolute"></div>
+      <div className="w-[42vw] bg-black h-10 mt-2 right-0 absolute"></div>
+    </>
+  );
+}
+
+export default Header;
