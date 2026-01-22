0b68fcfb3487b15b2cdd5c57fdf883474c3914d5
IMAMSB6409 <imam0406@student.abudzar.sch.id>
2026-01-22 11:17:24 +0700

    updat

--- src/components/container/Header.jsx
+++ src/components/container/Header.jsx
@@ -1,9 +1,13 @@
+import TBHX from '../../assets/tbhx.webp';
+
 function Header() {
+
   return (
-    <>
-      <div className="w-[42vw] bg-black h-10 mt-2 left-0 absolute"></div>
-      <div className="w-[42vw] bg-black h-10 mt-2 right-0 absolute"></div>
-    </>
+    <div className='flex mt-2 justify-center items-center w-full h-max'>
+      <div className="w-[42vw] bg-black h-10  left-0 absolute"></div>
+      <div className="w-[42vw] bg-black h-10  right-0 absolute"></div>
+      <img src={TBHX} className='w-[192px]' alt="" />
+    </div>
   );
 }
 
