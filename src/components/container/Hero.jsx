855ae361f5f12ac00b9933c87dbbb80181ac814e
zhafran0408 <zhafran1108@student.abudzar.sch.id>
2026-01-23 09:48:30 +0700

    salaj

--- src/components/container/Hero.jsx
+++ src/components/container/Hero.jsx
@@ -4,7 +4,7 @@ import CardList from '../Perintilan/CardList';
 function Hero() {
   return (
     <section className="">
-      <img src={heroImg} className="fixed top-[200px] -z-10 opacity-5"  alt="" />
+      <img src={heroImg} className="fixed lg:top-[100px] xl:top-[200px] -z-10 opacity-20"  alt="" />
  <CardList />
     </section>
   );
