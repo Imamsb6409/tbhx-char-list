c22ea354d47b88756068c7796edffe0c47074d87
IMAMSB6409 <imam0406@student.abudzar.sch.id>
2026-01-23 10:31:29 +0700

    asa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/container/Header";
 import Hero from './components/container/Hero';
+import Footer from './components/container/Footer';
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Footer />
     </>
   );
 }
---
+++ src/components/container/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Footer() {
+    return (
+        <footer className="w-full py-4 flex justify-center items-center border-t mt-8">
+            <p className="text-sm text-gray-600">
+                &copy; 2024 TBHX Character List. All rights reserved.
+            </p>
+        </footer>
+    );
+}
